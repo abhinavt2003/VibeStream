a616db833c79b46003c5a507f3d3e8096edd899c
abhinavt1606 <134359874+abhinavt2003@users.noreply.github.com>
2024-05-20 19:18:01 +0530

    file system multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,24 @@
+// using multer we will create middlewares
+// Middlewares can be written directly too
+// jaate time mil k jana - Middleware
+// file upload ki jrurat hogi there I will use multer
+// We have to configure middleware
+
+import multer from "multer";
+
+
+// We can use disk Storage,memory Storage but we are using our disk storage 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.originalname)  //bad practice, will give many files, overwritten //original file return kr dega
+    }
+})
+
+export const upload = multer({
+     storage, 
+})
